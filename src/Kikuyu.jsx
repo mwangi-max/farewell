420c410454f3902b18340df99ec897c592195c64
Kelvin Kamau <kelvinkam24@gmail.com>
2024-11-21 09:17:03 +0300

    changed height of the image

---
+++ src/Kikuyu.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import picture from './assets/WAWERU.jpg'
+import './eulogy.css'
+
+const Kikuyu = () => {
+  return (
+    <article className='first'>
+        <section className='kwanza'>
+            <div className='head'>
+            <h2 >EULOGY OF THE LATE</h2>
+            <h3 style={{textAlign:'center', fontStyle:'italic', color:'green', fontWeight:'bolder', fontSize:'22px'}}>CHARLES WAWERU KAMAU</h3>
+            </div>
+           
+            <figure className='figa'>
+           <img src={picture} alt='WAWERU' className='picture'/>
+           </figure>
+           <p style={{textAlign:'center', fontStyle:'italic', color:'green', fontWeight:'bolder'}}>Sunrise: 10th October 1971</p>
+           <p style={{textAlign:'center', fontStyle:'italic', color:'green', fontWeight:'bolder'}}>Sunrise: 16th November 2024</p>
+        </section>
+        <section>
+           <h2>RUGANO RWA MWENDO-NIIRI CHARLES WAWERU KAMAU.</h2> 
+<h3>GUCIARWO:</h3>
+<p>Mwendo-niiri Charles Waweru Kamau aciarirwo mweri ikumi, mweri wa ikumi, mwaka-ini wa 1971 (10th October 1971), ituraiini ria Mombatha, Karuri ward, Kiambaa sub-county, Kiambu County.</p>
+<p>Aciari ake ni Mary Wairimu na mutiga-iri John Kamau Waweru.</p>
+<p>Maciaranirwo na Assistant Chief Elizabeth Nyambura, Joseph Ngugi, mutiga-iri Samson Ngumi, David Njoka, na Kelvin Mwangi (Kiambu County Government).</p>
+
+<h2>GITHOMO:</h2>
+<p>Niambiriirie thukuru thukuru-ini wa Karuri Primary School mwaka-ini wa 1979 na akirikia na gwika kigeranio gia KCPE mwaka-ini wa 1986. Niathire na mbere na githomo Karuri High School na akirikia na gwika kigeranio gia KCSE , mwaka-ini wa 1991.</p>
+
+<h2>KURUA:</h2>
+<p>Mwendo-niiri niaruire mwaka-ini wa 1986.</p>
+
+<h2>KANITHA:</h2>
+<p>Waweru ni abatithirio kanitha-ini wa Catholic Church Karuri, mwaka-ini wa 1986 arugamiriri ake mari Mr. & Mrs. Peter Mbere. Abatithirio ni Father mutiga-iri Joseph Richetti.</p>
+
+<h2>WIRA:</h2>
+<p>Mwendo-niiri niarutaga mawira maciibarua Banana, Karuri. Thutha ucio niathamiire gwa baba wao mukuru, Samson Ngumi, Nairobi, Baba Ndogo kuria araikaraga nginya kuhuruka gwake.</p>
+
+<h2>NDWARI:</h2>
+<p>Waweru niambiririe guthinio ni mwiri mweri wa ikumi mwaka-ini uyu. Niakomire thibitari wa Mbagathi District Hospital Nairobi, kuria akomire gwa kiumia kimwe agichoka akiinuka. Kuri mweri ikumi na ithathatu mweri wa ikumi na umwe (16th November 2024) mathaa-ini ma muthenya ni agiire na thina wa kuhihia na akuguthukio thibitari ya Kiambu Level V (5) Hospital.</p>
+
+<h2>KUHURUKA:</h2>
+<p>Muthenya oro ucio (16th November 2024) agithaitirwo na madigitari ni ahurukirio ni mwathani kuri thaa ikumi na ndagika mirongo iri na ithano (4:25 pm). Arahurukire ena ukuru wa miaka mirongo itano na itatu (53 years).</p>
+
+<h2>Charles Waweru Kamau, Ngai arokuhe kihuruko kiega.</h2>
+
+
+        </section>
+
+        <section>
+            <h2>JESU WEE UNYENDETE</h2>
+            <p>Jesũ We ũnyendete,</p>
+<p>Nĩngũũrĩra harĩ we:</p>
+<p>Rĩrĩa mbaara ĩĩ nene,</p>
+<p>Nowe wa kũ’honokia;</p>
+<p>’Hitha mbaru-inĩ ciaku,</p>
+<p>Niĩ mũndũ mũthĩni;</p>
+<p>’Honokia Ngai wakwa</p>
+<p>Gitĩra muoyo wakwa.</p>
+<div className='spacer'></div>
+<p>Wĩhitho wakwa nowe</p>
+<p>Na murũ wakwa nowe;</p>
+<p>Ndikaingatwo harĩ we,</p>
+<p>Njoya ũnjĩtĩkĩre;</p>
+<p>Niĩ nĩngwĩhokete,</p>
+<p>Nowe ũteithio wakwa</p>
+<p>O na mwĩgitio wakwa;</p>
+<p>Ũndoorerie thũ ciakwa.</p>
+<div className='spacer'></div>
+<p>Wega wothe nĩ waku,</p>
+<p>Wohanĩri no waku;</p>
+<p>Mwathani ndanyihĩrũo,</p>
+<p>Njiyũria wega waku:</p>
+<p>Jesu ndakwamũkĩra,</p>
+<p>We Githima kĩa muoyo;</p>
+<p>Therũka thĩinĩ wakwa</p>
+<p>Kinya tene na tene.</p>
+
+<h2>MIKURU YARIKA:</h2>
+<p>Jesũ nĩngenaga nĩ wendo ũcio waku</p>
+<p>Ũgĩtĩkĩra kũnguĩra o na itarĩ kĩene</p>
+<p>Ngĩe kĩrĩgĩrĩro kĩrĩa ĩtarĩ mbere</p>
+<p>Gĩa thũmbĩ ĩrĩa ũnjigĩire ĩrĩa ĩtabuthaga</p>
+<div className='spacer'></div>
+<p>Mĩkuru yarika na ĩrĩma cia neneha</p>
+<p>Ndeithia Ngai wakwa ndikanorĩre kuo</p>
+<p>Njerũhĩria hinya tuĩka mũndongoria</p>
+<p>Na ũndeithie gũkwĩhoka o na kũrĩ nduma</p>
+<div className='spacer'></div>
+<p>Nĩwakirĩrĩirie hau mũtharaba-inĩ</p>
+<p>Ũkĩamwo nĩgetha ngainia thũmbĩ ĩo ya muoyo</p>
+<p>Kĩhe ũkirĩrĩria rũgendo-inĩ rũrũ</p>
+<p>Na ũhe ngo ya wĩtĩkio ndũrĩkĩtie kĩrĩti</p>
+<div className='spacer'></div>
+<p>Rĩmwe nĩnogaga na ngagĩa na nganja</p>
+<p>Ngona rũgendo rũa raiha na ngorũo nĩ hinya</p>
+<p>No ngaigua mũgambo wa Jesũ Mũnyendi</p>
+<p>Ũkĩnjĩra nyũmĩrĩrie tondũ akirie gũka</p>
+<div className='spacer'></div>
+<p>Ngai ũrĩaa warĩ ho hĩndĩ ya Daniel</p>
+<p>O na riu arĩ na hinya ndagarũrũkaga</p>
+<p>We nĩ mwĩhokeku ndagarũrũkaga</p>
+<p>O na hĩndĩ ya mathĩna ndagarũrũkaga</p>
+<div className='spacer'></div>
+<p>Nawe ũrĩ rũgendo-inĩ ndũkorũo nĩ hinya</p>
+<p>Mbaara na ikeno cia thĩ ĩno ikirie gũthira</p>
+<p>Jesũ akirie gũka agatuhurũkie</p>
+<p>Kũrĩa gũtarĩ maithori na gũtarĩ thĩna</p>
+        </section>
+    </article>
+  )
+}
+
+export default Kikuyu
